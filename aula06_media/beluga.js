323a5a0345de9c5bcd94b9f66113c33947c38144
Wagner Pereira dos Santos <wpereira@lnxcit017506.cit>
2019-01-04 09:35:25 -0200

    Change test

--- aula06_media/beluga.js
+++ aula06_media/beluga.js
@@ -1,13 +1,14 @@
-function calcularMedia(){                    
-                    
+function calcularMedia(){
+
     var total = 0;
     var qtd = arguments.length;
     var x = 0;
+
     //while(arguments[x]){
     while(typeof arguments[x] === 'number'){
         total += arguments[x];
         x++
-    }                    
+    }
 
     return total / qtd;
 }

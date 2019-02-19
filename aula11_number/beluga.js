b2481be332748fbdaa84c80c63056331e4397ab1
wpereira <wpereira@ciandt.com>
2019-02-18 22:47:11 -0300

    [DESAFIO-AULA-11] - Solution implementation - DONE

--- aula11_number/beluga.js
+++ aula11_number/beluga.js
@@ -1,14 +1,13 @@
-function calcularMedia(){
-
+function calcularMedia(){                    
+                    
     var total = 0;
     var qtd = arguments.length;
     var x = 0;
-
     //while(arguments[x]){
     while(typeof arguments[x] === 'number'){
         total += arguments[x];
         x++
-    }
+    }                    
 
     return (total / qtd).toFixed(2);
 }
